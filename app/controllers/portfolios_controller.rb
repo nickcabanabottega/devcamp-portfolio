a7be9b20a8faa6315243d13762e6cb217a9aff5d
Nick Cabana <cabanacleaningservices@gmail.com>
2017-06-16 23:45:29 -0600

    Implemented custom scopes for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,10 @@ class PortfoliosController < ApplicationController
 		@portfolio_items = Portfolio.all
 	end
 
+	def angular
+		@angular_portfolio_items = Portfolio.angular
+	end
+
 	def new
 		@portfolio_item = Portfolio.new
 	end
@@ -52,4 +56,5 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
    end
   end
+
 end
